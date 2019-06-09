31f48dd5bfb226f1dbf2244d6350773f9a81bebd
pixelyanyu <1046425884@qq.com>
2019-06-09 16:05:00 +0800

    123

--- app/src/main/java/com/bawei/myapplication/MainActivity.java
+++ app/src/main/java/com/bawei/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //修改
         ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> lis1t = new ArrayList<>();
     }
 }
